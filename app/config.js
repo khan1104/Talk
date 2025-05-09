5bae4930e97c97f160018151c22277c052a09138
khan1104 <149855540+khan1104@users.noreply.github.com>
2025-05-09 17:09:36 +0530

    Update config.js

--- app/config.js
+++ app/config.js
@@ -4,13 +4,7 @@ import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
     // Apne Firebase config yahan dalen
-    apiKey: "AIzaSyDK_o_mt2yHhlHt-Yopiuta-hkrH49LOig",
-    authDomain: "talk-6b03e.firebaseapp.com",
-    projectId: "talk-6b03e",
-    storageBucket: "talk-6b03e.appspot.com",
-    messagingSenderId: "896911416885",
-    appId: "1:896911416885:web:48899b33b9060d12b91b7c",
-    measurementId: "G-W2855Q5Q77"
+  
 };
 
 const app = initializeApp(firebaseConfig);
